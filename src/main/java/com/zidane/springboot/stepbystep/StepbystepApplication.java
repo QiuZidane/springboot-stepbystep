cf42c1b4d8d56696c074157857250866162a8768
QZidane <qiusdsdc@126.com>
2018-11-14 22:15:57 +0800

    init project

---
+++ src/main/java/com/zidane/springboot/stepbystep/StepbystepApplication.java
@@ -0,0 +1,12 @@
+package com.zidane.springboot.stepbystep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepbystepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepbystepApplication.class, args);
+    }
+}
